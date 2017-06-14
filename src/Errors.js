92cbad0465713e2c666da43051486c1f45f1056e
Mavmeister <jimbobthedrummer@gmail.com>
2017-06-13 21:31:10 -0700

    Added error messages in separate file

--- src/Errors.js
+++ src/Errors.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 const Errors = {
 	savingsTooLow: 'You should have at least 3 months worth of your total expenses in savings',
 	savingsTooHigh: 'You have too much in your savings, the max should be 6 months worth of your total expenses. [Put that extra money to work in investments. You can use a service like Betterment.com][Pay down your debt with the highest interest rate]',
-
-
-
-
+	fourKlow: 'When you retire, you should look to have 10-12x your annual salary in your retirement fund. Do do that you should have at least a 10% contribution rate.',
+	fourKonTrack: 'You’re on track to hit 10-12x your salary in your retirement fund, which is where you want to be as long as it is 100% invested',
+	fourKhigh: 'The most you can contribute to your 401k is $18,000 a year. You should look to move that into an IRA',
+	retirementGood: 'Great to contribute to other retirement accounts, keep it up!',
+	retirementHigh: 'The max you can contribute every year to an IRA is $5,500 unless you’re over 50, which you can contribute $6,500',
+	investmentBad: 'You may want to think about [adding more to your savings] [contributing at least 10% to your 401k] before having it in general investements',
+	investmentHighDebt: 'You should pay off your debt above 5% before investing as you should not expect to make more than that in your investments annually',
+	investmentGood: 'All other buckets are taking care of so great to have your money working for you',
+	debtHigh: 'Pay off the debt with the highest rate as fast as possible',
+	debtLow: 'If this debt is not used to help with an investment than it should be paid off, otherwise some debt with low interest rates are okay',
 
 }
 
